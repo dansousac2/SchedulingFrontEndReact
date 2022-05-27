4da864cac46cf04be22b7bb59667cab1f34f0d93
DanSousa <dansousac2@gmail.com>
2022-05-27 12:19:55 -0300

    create Place and Sport done

--- src/screens/Place/CreateP/CreatePlace.js
+++ src/screens/Place/CreateP/CreatePlace.js
@@ -6,19 +6,19 @@ import axios from "axios";
 
 export default class CreatePlace extends React.Component {
     state = {
-        eventName:"",
-        date:"",
-        address:"",
-        budget:""
+        placeName:"",
+        placeReference:"",
+        capacityMax:"",
+        isPublic:""
     }
     
     post = () => {
-        axios.post( 'http://localhost:8080/api/event/save',
+        axios.post( 'http://localhost:8080/api/place',
             {
-                eventName: this.state.eventName,
-                date: this.state.date,
-                adress: this.state.address,
-                budget: this.state.budget
+                name: this.state.placeName,
+                reference: this.state.placeReference,
+                maximumCapacityParticipants: this.state.capacityMax,
+                public: this.state.isPublic
             }
         ).then( Response => {
             console.log(Response)
@@ -32,22 +32,22 @@ export default class CreatePlace extends React.Component {
             <div>
                 <header className="App-header">
                     <fieldset>
-                        <legend><h2>Create Event</h2></legend>
-                        <FormGroup label='Event Name' htmlFor='lab01'>
+                        <legend><h2>Create Place</h2></legend>
+                        <FormGroup label='Place Name' htmlFor='lab01'>
                             <input className="form-control form-control-lg" type="text" placeholder="name" id="lab01"
-                            onChange={(e) => {this.setState({eventName: e.target.value})}}/>
+                            onChange={(e) => {this.setState({placeName: e.target.value})}}/>
                         </FormGroup>
-                        <FormGroup label='Event Date' htmlFor='lab02'>
-                            <input className="form-control form-control-lg" type="text" placeholder="date" id="lab02"
-                            onChange={(e) => {this.setState({date: e.target.value})}}/>
+                        <FormGroup label='Place Reference' htmlFor='lab02'>
+                            <input className="form-control form-control-lg" type="text" placeholder="reference" id="lab02"
+                            onChange={(e) => {this.setState({placeReference: e.target.value})}}/>
                         </FormGroup>
-                        <FormGroup label='Event Address' htmlFor='lab03'>
-                            <input className="form-control form-control-lg" type="text" placeholder="address" id="lab03"
-                            onChange={(e) => {this.setState({address: e.target.value})}}/>
+                        <FormGroup label='Place Capacity' htmlFor='lab03'>
+                            <input className="form-control form-control-lg" type="text" placeholder="capacity" id="lab03"
+                            onChange={(e) => {this.setState({capacityMax: e.target.value})}}/>
                         </FormGroup>
-                        <FormGroup label='Event Budget' htmlFor='lab04'>
-                            <input className="form-control form-control-lg" type="text" placeholder="address" id="lab04"
-                            onChange={(e) => {this.setState({budget: e.target.value})}}/>
+                        <FormGroup label='Public Place?' htmlFor='lab04'>
+                            <input className="form-control form-control-lg" type="text" placeholder="is public? (true/false)" id="lab04"
+                            onChange={(e) => {this.setState({isPublic: e.target.value})}}/>
                         </FormGroup>
                         <br/>
                         <button type="button" className="btn btn-primary btn-lg" onClick={this.post} >Create</button>
--- src/screens/Sport/CreateS/CreateSport.js
+++ src/screens/Sport/CreateS/CreateSport.js
@@ -6,19 +6,13 @@ import axios from "axios";
 
 export default class CreateSport extends React.Component {
     state = {
-        eventName:"",
-        date:"",
-        address:"",
-        budget:""
+        sportName:""
     }
     
     post = () => {
-        axios.post( 'http://localhost:8080/api/event/save',
+        axios.post( 'http://localhost:8080/api/sport',
             {
-                eventName: this.state.eventName,
-                date: this.state.date,
-                adress: this.state.address,
-                budget: this.state.budget
+                name: this.state.sportName
             }
         ).then( Response => {
             console.log(Response)
@@ -32,22 +26,10 @@ export default class CreateSport extends React.Component {
             <div>
                 <header className="App-header">
                     <fieldset>
-                        <legend><h2>Create Event</h2></legend>
-                        <FormGroup label='Event Name' htmlFor='lab01'>
+                        <legend><h2>Create Sport</h2></legend>
+                        <FormGroup label='Sport Name' htmlFor='lab01'>
                             <input className="form-control form-control-lg" type="text" placeholder="name" id="lab01"
-                            onChange={(e) => {this.setState({eventName: e.target.value})}}/>
-                        </FormGroup>
-                        <FormGroup label='Event Date' htmlFor='lab02'>
-                            <input className="form-control form-control-lg" type="text" placeholder="date" id="lab02"
-                            onChange={(e) => {this.setState({date: e.target.value})}}/>
-                        </FormGroup>
-                        <FormGroup label='Event Address' htmlFor='lab03'>
-                            <input className="form-control form-control-lg" type="text" placeholder="address" id="lab03"
-                            onChange={(e) => {this.setState({address: e.target.value})}}/>
-                        </FormGroup>
-                        <FormGroup label='Event Budget' htmlFor='lab04'>
-                            <input className="form-control form-control-lg" type="text" placeholder="address" id="lab04"
-                            onChange={(e) => {this.setState({budget: e.target.value})}}/>
+                            onChange={(e) => {this.setState({sportName: e.target.value})}}/>
                         </FormGroup>
                         <br/>
                         <button type="button" className="btn btn-primary btn-lg" onClick={this.post} >Create</button>
