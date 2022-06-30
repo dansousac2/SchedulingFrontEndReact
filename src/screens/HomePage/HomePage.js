7384133d3c80550020b644394508b067a55ed757
Ayanne Prata <ayanneprata7@gmail.com>
2022-06-30 19:04:20 -0300

    Update in home page

--- src/screens/HomePage/HomePage.js
+++ src/screens/HomePage/HomePage.js
@@ -14,7 +14,7 @@ export default class HomePage extends React.Component {
             <div className="HomePage1">
                 <h1>Sejam Bem Vindo(a)s!</h1>
                 <h2>SAPE</h2>
-                <h3>O SAPE é um sistema voltado para funcionários, alunos e professores do IFPB - Campus Monteiro, tendo como objetivo facilitar o gerenciamento e programação das atividades físicas praticadas na instituição. No SAPE, a comunidade acadêmica pode agendar a prática de determinados exercícios físicos no campus, consultar dias e horários dos mesmas e interessados poderão confirmar presença na prática.</h3>
+                <h3>O SApe é um sistema voltado para funcionários, alunos e professores do IFPB - Campus Monteiro, tendo como objetivo facilitar o gerenciamento e programação das atividades físicas praticadas na instituição. No SAPE, a comunidade acadêmica pode agendar a prática de determinados exercícios físicos no campus, consultar dias e horários dos mesmas e interessados poderão confirmar presença na prática.</h3>
                 <br/>
                 <button type="button" className="btn btn-primary" onClick={this.registerPractice} >Cadastrar Prática</button>      
             </div>
