9ab063e20141b9a7d9864f114dc778653be37122
Ayanne Prata <ayanneprata7@gmail.com>
2022-07-21 23:15:08 -0300

    ApiService.js changes required for authentication

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,13 +1,31 @@
 import axios from "axios";
 
-const httpClient = axios.create ({
-    baseURL:'http://localhost:8080/api'
-})
+import StorageService from "./StorageService";
+
+export const LOGGED_USER = 'loggedUser';
+export const TOKEN = 'token';
+
+const baseURL = process.env.REACT_APP_API_URL;
+
+export const httpClient = axios.create ({
+    baseURL:'http://localhost:8080/api',
+    withCredentials: true,
+});
 
 export default class ApiService {
     
     constructor(endpoint) {
         this.endpoint = endpoint;
+
+        this.storageService = new StorageService();
+        const token = this.storageService.getItem(TOKEN);
+        this.registerToken(token);
+    }
+
+    registerToken(token){
+        if(token){
+            httpClient.defaults.headers.common['Authorization'] = `Bearer ${token}`
+        }
     }
 
     post(url, params) {
