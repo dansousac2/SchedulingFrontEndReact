d1d055ffcf2bf8ee2453eccd52ffb09a2898051d
Ayanne Prata <ayanneprata7@gmail.com>
2022-07-23 18:39:21 -0300

    Changes to NavbarItem required for authentication

--- src/componentes/NavbarItem.js
+++ src/componentes/NavbarItem.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React from "react"
 
-function NavbarItem(props){
-    return(
-        <li className="nav-item">
-            <a className="nav-link" href={props.href}>{props.label}</a>
-        </li>
-    )
+function NavbarItem( {render, ...props}){
+    if(render){
+        return (
+            <li className="nav-item">
+                <a className="nav-link" onClick={props.onClick} href={props.href}>{props.label}</a>
+            </li>
+
+        )
+    }else{
+        return false;
+
+    }
+
 }
 
 export default NavbarItem;
