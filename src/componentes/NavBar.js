e77699542f63b4936fc28607d78a93044c3fdab4
Ayanne Prata <ayanneprata7@gmail.com>
2022-07-23 20:52:42 -0300

    Changes to NavBar.js needed for authentication

--- src/componentes/NavBar.js
+++ src/componentes/NavBar.js
@@ -1,5 +1,7 @@
 import React from "react";
-import NavbarItem from './NavbarItem'
+import NavbarItem from './NavbarItem';
+import { AuthConsumer } from '../main/SessionProvider';
+
 
 function NavBar (props) {
     return (
@@ -12,16 +14,17 @@ function NavBar (props) {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarResponsive"> 
                     <ul className="navbar-nav">
-                        <NavbarItem href="/login" label="Login" />
+                        <NavbarItem render={!props.isAuthenticated} href="/login" label="Login" />
+                        <NavbarItem render={props.isAuthenticated} href="/login" onClick={props.logout} label="Sair" />
 
-                        <NavbarItem href="/listPlaces" label="Locais" />
-                        <NavbarItem href="/createPlace" label="Criar Local" />
+                        <NavbarItem render={props.isAuthenticated} href="/listPlaces" label="Locais" />
+                        <NavbarItem render={props.isAuthenticated} href="/createPlace" label="Criar Local" />
 
-                        <NavbarItem href="/listSports" label="Esportes" />
-                        <NavbarItem href="/createSport" label="Criar Esporte" />
+                        <NavbarItem render={props.isAuthenticated} href="/listSports" label="Esportes" />
+                        <NavbarItem render={props.isAuthenticated} href="/createSport" label="Criar Esporte" />
                     
-                        <NavbarItem href="/listScheduling" label="Agendamentos" />
-                        <NavbarItem href="/createScheduling" label="Criar Agendamento" />
+                        <NavbarItem render={props.isAuthenticated} href="/listScheduling" label="Agendamentos" />
+                        <NavbarItem render={props.isAuthenticated} href="/createScheduling" label="Criar Agendamento" />
                     </ul>
                 </div>
             </div>
@@ -29,4 +32,10 @@ function NavBar (props) {
     )
 }
 
-export default NavBar;
+export default () => (
+    <AuthConsumer>
+        {(context) => (
+            <NavBar isAuthenticated={context.isAuthenticated} logout={context.end} />
+        )}
+    </AuthConsumer>
+)
