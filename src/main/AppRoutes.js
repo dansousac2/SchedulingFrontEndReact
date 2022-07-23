848adc0572638e6d2da8845b184393cc6f6c9d12
Ayanne Prata <ayanneprata7@gmail.com>
2022-07-23 18:26:16 -0300

    Changes to AppRoutes required for authentication

--- src/main/AppRoutes.js
+++ src/main/AppRoutes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, BrowserRouter} from 'react-router-dom';
+import {Route, BrowserRouter, Switch, Redirect} from 'react-router-dom';
 
 import HomePage from "../screens/HomePage/HomePage";
 import Login from "../screens/Login/Login";
@@ -15,25 +15,51 @@ import ListSports from "../screens/Sport/ListAllS/ViewSports";
 import CreateScheduling from "../screens/Scheduling/CreateSc/CreateSc";
 import ListScheduling from "../screens/Scheduling/ListAllSchedulings/ViewScheduling";
 
-function AppRoutes() {
+import { AuthConsumer } from "./SessionProvider";
+
+function RestrictedRoute( {component: Component, show, ...props} ) {
+    return(
+        <Route exact {...props} render={ (componentProps) => {
+            if(show){
+                return (
+                    <Component {...componentProps} />
+                )
+            }else{
+                return(
+                    <Redirect to={ {pathname : '/login', state : { from: componentProps.location}}} />
+                )
+            }
+        }} />
+    )
+
+}
+
+function AppRoutes(props) {
     return (
         <BrowserRouter>
+            <Switch>
             <Route component = { HomePage } path="/" exact/>
             <Route component = { Login } path="/login" />
 
-            <Route component = { ListPlaces } path="/listPlaces" />
-            <Route component = { CreatePlace } path="/createPlace" />
-            <Route component = { UpdatePlace } path="/updatePlace/:id" />
+            <RestrictedRoute show={props.isAuthenticated} component = { ListPlaces } path="/listPlaces" />
+            <RestrictedRoute show={props.isAuthenticated} component = { CreatePlace } path="/createPlace" />
+            <RestrictedRoute show={props.isAuthenticated} component = { UpdatePlace } path="/updatePlace/:id" />
      
-            <Route component = { ListSports } path="/listSports" />
-            <Route component = { CreateSport } path="/createSport" />
-            <Route component = { UpdateSport } path="/updateSport/:id" />
+            <RestrictedRoute show={props.isAuthenticated} component = { ListSports } path="/listSports" />
+            <RestrictedRoute show={props.isAuthenticated} component = { CreateSport } path="/createSport" />
+            <RestrictedRoute show={props.isAuthenticated} component = { UpdateSport } path="/updateSport/:id" />
            
 
-            <Route component = { ListScheduling } path="/listScheduling" />
-            <Route component = { CreateScheduling } path="/createScheduling" />
+            <RestrictedRoute show={props.isAuthenticated} component = { ListScheduling } path="/listScheduling" />
+            <RestrictedRoute show={props.isAuthenticated} component = { CreateScheduling } path="/createScheduling" />
+
+            </Switch>
         </BrowserRouter>
-    )
+    );
 }
 
-export default AppRoutes;
+export default () => (
+    <AuthConsumer>
+        { (context) => (<AppRoutes isAuthenticated={context.isAuthenticated} />) }
+    </AuthConsumer>
+)
